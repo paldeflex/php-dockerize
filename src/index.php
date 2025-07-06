ad45fc942ba4f1c01aedc2085ac41eaaff68b168
paldeflex <paldeflex@gmail.com>
2025-07-06 10:22:17 +0500

    Вывод сообщений об успехе в главном файле

--- src/index.php
+++ src/index.php
@@ -1,18 +1,30 @@
 <?php
 
-echo 'Everything is ok!';
+echo "PHP is working<br>";
 
-$host = '127.0.0.1';
-$db = 'php_db';
-$user = 'user';
-$pass = 'secret';
+if (extension_loaded('xdebug')) {
+    echo "Xdebug is enabled<br>";
+} else {
+    echo "Xdebug is not enabled<br>";
+}
+
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
-$pdo = new PDO($dsn, $user, $pass, $options);
 
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "MySQL connection successful<br>";
+} catch (PDOException $e) {
+    echo "MySQL connection failed: " . $e->getMessage();
+}

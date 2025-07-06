da47384f7b6820bb9c398dd144e05ac0b4bd7bce
paldeflex <paldeflex@gmail.com>
2025-07-06 09:45:43 +0500

    Поправил ошибку

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo 'Everything is ok!';
+
+$host = '127.0.0.1';
+$db = 'php_db';
+$user = 'user';
+$pass = 'secret';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
+
